bf2f1eb12b51a336390aa38432b183a23b38d1c7
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-08-03 21:47:03 -0400

    add hooks and toast messaging

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,42 +1,60 @@
 import {Flex, Button, Input} from '@chakra-ui/react'
 import './guessinput.css';
-import { currentCity  } from '../map/index';
+import MapboxMap, { currentCity  } from '../map/index';
 import React, {useState} from 'react';
-
-let text: String;
-
-const handleGuessSubmission = () => {
-
-    console.log(`Entered Text: ${text}`)
-    if (currentCity?.name == text) {
-        console.log("CORRECT")
-    }
-    else {
-        
-        console.log(`WRONG, correct answer was ${currentCity?.name}`)
-    }
-
-    text = "";
-}
+import { useToast } from '@chakra-ui/react'
 
 export function GuessInput() {
 
-    const [text, setValue] = React.useState('')
-    const handleChange = () => setValue(text)
+    let [value, setValue] = React.useState('')
+    const updateText = (event: { target: { value: React.SetStateAction<string>; }; }) => setValue(event.target.value)
+    const clearText = () => setValue("")
+    const toast = useToast()
     
+
+    const handleGuessSubmission = () => {
+
+        console.log(`Entered Text: ${value}`)
+        if (currentCity?.name.includes(value)) {
+            toast({
+                title: 'Correct!',
+                description: "We've created your account for you.",
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+              })
+            window.location.reload() // figure out how to rerender map
+        }
+        else {
+            
+            console.log(`WRONG, correct answer was ${currentCity?.name}`)
+            toast({
+                title: 'Wrong.',
+                description: "Read a book loser.",
+                status: 'error',
+                duration: 5000,
+                isClosable: true,
+              })
+        }
+
+        clearText();
+    }
+
     return (
         <Flex className="inputFlex"> 
             <Input
-                value = {text}
+                value={value}
                 textColor={"white"}
                 marginRight={15}
                 borderRadius={15}
                 backgroundColor={"#1A202C"}
-                onChange={handleChange}
+                onChange={updateText}
+
             />
             <Button
                 backgroundColor={"#1A202C"}
                 borderRadius={15}
+                variant={'solid'}
                 onClick={handleGuessSubmission}
             >
                 Guess
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,20 @@
 import './navbar.css';
 import { ColorModeSwitcher } from './ColorModeSwitcher'
-import { Box, Flex, Spacer } from '@chakra-ui/react'
+import { Box, Flex, Spacer, Button } from '@chakra-ui/react'
 
 export function NavBar() {
+
+    const refreshPage = ( () =>  { window.location.reload() })
+
     return (
         <nav className="navbar-container">
             <Flex height="5vh">
                 <Box className="navbar-box">
-                <h1>City Guesser</h1>
+                    <h1 onClick={refreshPage}>City Guesser</h1>
                 </Box>
                 <Spacer  height={1}/>
                 <Box className="navbar-box">
-                    <h1>Score: 100/100 </h1>
+                    <h1>0 Points</h1>
                 </Box>
             </Flex>
         </nav>
